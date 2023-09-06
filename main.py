d2a6d5493010fec934b0fc6241313d45a763593d
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2023-09-06 09:29:25 -0700

    Add files via upload
    Created a very barebones UI.

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import random
+from tkinter import *
+import hunter_data as hd
 
-def random_hunter_selection():
-    my_file = open("hunters.txt", "r")
-    data = my_file.read()
-    hunter_list = data.split("\n")
-    my_file.close()
-    selection = random.choice(hunter_list)
-    print(selection)
 
-random_hunter_selection()
+# ~~~ BASIC SETUP STUFF ~~~
+window = Tk()
+window.title("Rise up, dead man! A 'Hunt: Showdown' random hunter selector!")
+greeting = Label(text="Rise up, dead man!")
+greeting.pack()
+
+selection_prompt = Label(text="YOUR NEXT HUNTER IS:")
+selection_prompt.pack()
+
+your_choice = Label()
+
+# ~~~ List Box ~~~
+hunter_list = Listbox(selectmode=MULTIPLE)
+hunter_list.pack(side=LEFT,fill=BOTH)
+scrollbar = Scrollbar()
+scrollbar.pack(side=RIGHT,fill=BOTH)
+hunter_list.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=hunter_list.yview)
+for i in hd.compiled_hunter_list():
+    hunter_list.insert(END, i)
+
+# ~~~ THE BUTTON ~~~
+def the_button():
+    list_numbers = hunter_list.curselection()
+    selection_list = []
+    for i in list_numbers:
+        y = hunter_list.get(first=i)
+        selection_list.append(y)
+    selection_prompt.config(text=random.choice(selection_list))
+
+run_it = Button(
+    text="Choose your fate!",
+    width=20,
+    height=4,
+    command=the_button
+)
+run_it.pack()
+
+# ~~~ REQUIRED CODE ~~~
+window.mainloop()
